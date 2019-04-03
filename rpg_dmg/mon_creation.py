4436394a06aaa37a612e0266f2e4726ce87528f1
Brandon <brabick@gmail.com>
2019-04-03 17:55:56 -0600

    Revert "Create abstractClassDrill"
    This reverts commit 30880128929b4d35442ce91225f80ac34200eb1e.

--- rpg_dmg/mon_creation.py
+++ rpg_dmg/mon_creation.py
@@ -1,5 +1,5 @@
 import not_pokemon_func
-from not_pokemon_func import battle, mon, create_attack, super_effective, is_dead
+from not_pokemon_func import battle, dmg_calc
 
 #Attack creation
 # blastburn = not_pokemon_func.create_attack('Blast Burn', 'fire', 25)
@@ -22,13 +22,6 @@ from not_pokemon_func import battle, mon, create_attack, super_effective, is_dea
 # Venusaur = not_pokemon_func.mon('Venusaur', 'grass', None, 150, 10, 10, ven_attacks)
 
 
-#venblas = battle.turn1(battle, not_pokemon_func.Venusaur, not_pokemon_func.Blastoise, not_pokemon_func.ven_attacks, not_pokemon_func.blas_attacks)
-#ven_battle = battle(not_pokemon_func.Venusaur, not_pokemon_func.Blastoise, not_pokemon_func.ven_attacks, not_pokemon_func.blas_attacks)
-#ven_battle.battle.turn1(not_pokemon_func.Venusaur, not_pokemon_func.Blastoise, not_pokemon_func.ven_attacks, not_pokemon_func.blas_attacks)
+venblas = battle.turn1(battle, not_pokemon_func.Venusaur, not_pokemon_func.Blastoise, not_pokemon_func.ven_attacks, not_pokemon_func.blas_attacks)
 
 #battle.attack_selection(battle, not_pokemon_func.Blastoise)
-
-
-#print(not_pokemon_func.choose_attack.attack_selection(not_pokemon_func.choose_attack, not_pokemon_func.Venusaur, not_pokemon_func.ven_attacks))
-
-print(not_pokemon_func.char_attacks)
